9f91a650d0a287eb5b949a09e177091999ef631a
Silvio Merz <silvio.merz@dbs.ch>
2020-01-22 11:27:22 +0100

    Cleaned code

--- webcontent/includes/scripts/participate.js
+++ webcontent/includes/scripts/participate.js
@@ -42,37 +42,26 @@ function xmlHttpRequest() {
 }
 
 function showResult(surveyIndex, surveyId, surveyCount) {
-    const openedResults = $('div.poll1').length;
     xmlHttpRequest();
-    if (openedResults > 0) {
-        for (let i = 0; i < surveyCount; i++) {
-            if (i !== surveyIndex) {
-                let surveyIdToClose = i + 1;
-                xmlhttp.onreadystatechange = function () {
-                    if (this.readyState === 4 && this.status === 200) {
-                        document.getElementById("survey" + i).innerHTML = this.responseText;
-                    }
-                };
-                xmlhttp.open("GET", "includes/scripts/getQuestion.php?survey=" + surveyIdToClose, false);
-                xmlhttp.send();
-            } else {
-                xmlhttp.onreadystatechange = function () {
-                    if (this.readyState === 4 && this.status === 200) {
-                        document.getElementById("survey" + surveyIndex).innerHTML = this.responseText;
-                    }
-                };
-                xmlhttp.open("GET", "includes/scripts/getResult.php?survey=" + surveyId, false);
-                xmlhttp.send();
-            }
+    for (let i = 0; i < surveyCount; i++) {
+        if (i !== surveyIndex) {
+            let surveyIdToClose = i + 1;
+            xmlhttp.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200) {
+                    document.getElementById("survey" + i).innerHTML = this.responseText;
+                }
+            };
+            xmlhttp.open("GET", "includes/scripts/getQuestion.php?survey=" + surveyIdToClose, false);
+            xmlhttp.send();
+        } else {
+            xmlhttp.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200) {
+                    document.getElementById("survey" + surveyIndex).innerHTML = this.responseText;
+                }
+            };
+            xmlhttp.open("GET", "includes/scripts/getResult.php?survey=" + surveyId, false);
+            xmlhttp.send();
         }
-    } else {
-        xmlhttp.onreadystatechange = function () {
-            if (this.readyState === 4 && this.status === 200) {
-                document.getElementById("survey" + surveyIndex).innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET", "includes/scripts/getResult.php?survey=" + surveyId, true);
-        xmlhttp.send();
     }
 }
 
