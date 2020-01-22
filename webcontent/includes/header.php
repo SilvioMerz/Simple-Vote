9efad4d00730e8e3684092ab00b345178a88c6c9
SilvioMerz <silvio.merz01@gmail.com>
2020-01-22 20:15:47 +0100

    Edited header styling

--- webcontent/includes/header.php
+++ webcontent/includes/header.php
@@ -1,5 +1,7 @@
 <link rel="stylesheet" type="text/css" href="../styles/header.css">
-<header class="jumbotron">
-    <img src="../Documents/logo2.PNG" alt = "Selfhtml">
-    <h1><?php echo $title ?></h1>
+<header>
+    <div id="content-middle-of-height">
+        <a href="index.php"><h1><?php echo $title ?></h1></a>
+        <a href="index.php"><img src="../Documents/logo2.PNG" alt="Selfhtml"></a>
+    </div>
 </header>
