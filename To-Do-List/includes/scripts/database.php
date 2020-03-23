69f788b3a9bbde2d77f8062360c70561d4d49368
Silvio Merz <silvio.merz@dbs.ch>
2020-03-23 16:10:26 +0100

    Own file for toastr

--- To-Do-List/includes/scripts/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$con = new mysqli("localhost", "root", "", "to-do-list-database");
-if ($con->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
-}
